a9741e40c5598a1e5c9cc073da959b81a44c351b
KietPham-VN <phamanhkiet.dev@gmail.com>
2025-02-24 21:42:49 +0700

    IQueryable

--- Application/Services/IStudentService.cs
+++ Application/Services/IStudentService.cs
@@ -18,28 +18,41 @@ namespace ToDoApp.Application.Services
 		}
 		public IEnumerable<StudentViewModel> GetStudents(int? SchoolId)
 		{
-			var students = _context.Student.Include(student => student.School);
+			// query : select * From Student 
+			// Join School on Student.SchoolId = School.Id
+
+			var students = _context.Student.Include(student => student.School)
+				.AsQueryable();
 
 			if (SchoolId.HasValue)
 			{
-				return students.Where(student => student.School.Id == SchoolId)
-					.Select(student => new StudentViewModel
-					{
-						Id = student.Id,
-						FullName = student.FristName + " " + student.LastName,
-						Age = student.Age,
-						SchoolName = student.School.Name
-					}).ToList();
+				// query : select * From Student 
+				// Join School on Student.SchoolId = School.Id
+				// Where School.Id = SchoolId
+				students = students.Where(student => student.School.Id == SchoolId);
+					
 			}
-			return students.Select(student => new StudentViewModel
+			// IQueryable là 1 interface kế thừa từ IEnumerable
+			// đại diện cho 1 tập hợp các phần tử có thể truy vấn
+
+			// SELECT Student.Id,
+			// Student.FristName + Student.LastName AS FullName,
+			// Student.Age,
+			// School.Name AS SchoolName
+			// FROM Student
+			// JOIN School ON Student.SchoolId = School.Id
+			// WHERE School.Id = SchoolId (nếu schoolid bị null)
+
+			// tới khúc này khi được .toList() thì query mới được chạy nè
+			// nếu không thì nó chỉ là 1 câu lệnh truy vấn chưa được chạy
+			// khúc này chắc chắc bị hỏi
+			return [.. students.Select(student => new StudentViewModel
 			{
 				Id = student.Id,
 				FullName = student.FristName + " " + student.LastName,
 				Age = student.Age,
 				SchoolName = student.School.Name
-			}).ToList();
+			})];
 		}
-
-
 	}
 }
